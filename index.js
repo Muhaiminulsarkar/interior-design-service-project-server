4498e2349dd0f3e837ee1a1bcac26c26acc8c07e
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2022-11-11 21:08:51 +0600

    added cors

--- index.js
+++ index.js
@@ -11,14 +11,6 @@ app.use(express.json());
 
 console.log(process.env.DB_USER);
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1xnwouw.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri);
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   serverApi: ServerApiVersion.v1,
-// });
-
 
 
 
@@ -144,7 +136,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`cloud kitchen server is running ${port}`);
+  console.log(`cloud kitchen server ${port}`);
 });
 
 
